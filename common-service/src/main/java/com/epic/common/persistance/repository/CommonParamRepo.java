537957841678214d9c2e30088d2c8f4d14811d95
pemitha_r <pemitha_r@epiclanka.net>
2022-07-04 09:45:22 +0530

    Common Service Initial Commit

---
+++ common-service/src/main/java/com/epic/common/persistance/repository/CommonParamRepo.java
@@ -0,0 +1,17 @@
+package com.epic.common.persistance.repository;
+
+import com.epic.common.models.CommonParamBean;
+import com.epic.common.persistance.entity.ShCommonParam;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommonParamRepo extends JpaRepository<ShCommonParam,String> {
+
+    @Query("select new com.epic.common.models.CommonParamBean(s.id,s.paramValue,s.description) from ShCommonParam s where s.category = ?1")
+    List<CommonParamBean> getUnderCategory(String category);
+
+
+}
